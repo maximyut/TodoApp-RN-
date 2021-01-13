02c056fda5bd939230a2a3a719b443f1cc8eeffd
maximyut <garfonze1@gmail.com>
2021-01-13 13:35:44 +0300

    fetch

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,17 +1,35 @@
-import React, { useState, useEffect, useContext } from "react";
-import { StyleSheet, View, FlatList, Image, Dimensions } from "react-native";
+import React, { useState, useEffect, useContext, useCallback } from "react";
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  Image,
+  Dimensions,
+  Button,
+} from "react-native";
 import { AddTodo } from "../components/AddToDo";
 import { Todo } from "../components/Todo";
 import { AppTitle } from "../components/ui/AppTitle";
+import { AppText } from "../components/ui/AppText";
 import { ScreenContext } from "../context/screen/screenContext";
 import { TodoContext } from "../context/todo/todoContext";
+import { AppLoader } from "../components/ui/AppLoader";
+import { THEME } from "../theme";
 
 export const MainScreen = ({}) => {
-  const { addTodo, todos, removeTodo } = useContext(TodoContext);
+  const { addTodo, todos, removeTodo, fetchTodos, loading, error } = useContext(
+    TodoContext
+  );
   const { changeScreen } = useContext(ScreenContext);
   const [deviceWidth, setDeviceWodth] = useState(
     Dimensions.get("window").width - 50
   );
+
+  const loadTodos = useCallback(async () => await fetchTodos(), [fetchTodos]);
+
+  useEffect(() => {
+    loadTodos();
+  }, []);
 
   useEffect(() => {
     const update = () => {
@@ -25,6 +43,22 @@ export const MainScreen = ({}) => {
       Dimensions.removeEventListener("change", update);
     };
   });
+
+  if (loading) {
+    return <AppLoader />;
+  }
+  if (error) {
+    return (
+      <View style={styles.center}>
+        <AppText style={styles.error}>{error}</AppText>
+        <Button
+          title="Повторить"
+          color={THEME.MAIN_COLOR}
+          onPress={loadTodos}
+        />
+      </View>
+    );
+  }
 
   let content = (
     <View style={{ width: deviceWidth }}>
@@ -74,5 +108,14 @@ const styles = StyleSheet.create({
     width: "100%",
     height: "100%",
     resizeMode: "contain",
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  error: {
+    fontSize: 30,
+    color: THEME.DANGER_COLOR,
   },
 });
